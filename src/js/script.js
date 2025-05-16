bae574817423247e3f476b22a26d8b213cf17f3b
mar <137989121+mar5608@users.noreply.github.com>
2025-05-16 15:07:08 +0900

    aboutus対応中

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,10 @@ jQuery(function ($) {
   // この中であればWordpressでも「$」が使用可能になる
 });
 
+/************************
+ * Splide
+ ************************/
+
 //Splideの初期化
 const fvSplide = new Splide(".js-fv-splide").mount();
 
@@ -26,3 +30,41 @@ let splideOption = {
 new Splide(".js-fv-splide", splideOption).mount();
 
 const campaignSplide = new Splide(".js-campaign-splide").mount();
+
+/************************
+ * drawer
+ ************************/
+jQuery("#js-drawer-icon").on("click", function (e) {
+  e.preventDefault();
+  jQuery("#js-drawer-icon").toggleClass("is-checked");
+  jQuery("#js-drawer-content").toggleClass("is-checked");
+});
+
+/** スムーススクロール*/
+/** drawer 制御 */
+jQuery('#js-drawer-content a[href^="#"]').on("click", function (e) {
+  e.preventDefault();
+  jQuery("#js-drawer-icon").removeClass("is-checked");
+  jQuery("#js-drawer-content").removeClass("is-checked");
+});
+
+jQuery('a[href^="#"]').on("click", function (e) {
+  const speed = 300;
+  const id = jQuery(this).attr("href");
+  const target = jQuery("#" == id ? "html" : id);
+  const position = jQuery(target).offset().top;
+  jQuery("html,body").animate(
+    {
+      scrollTop: position,
+    },
+    speed,
+    "swing" // swing or liner
+  );
+  // jQuery("html,body").animate(
+  //   {
+  //     scrollTop: 0,
+  //   },
+  //   1000,
+  //   "swing" // swing or liner
+  // );
+});
