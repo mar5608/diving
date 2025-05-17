ca4b1fa157ca03426c179c946c08abeed91082b6
mar <137989121+mar5608@users.noreply.github.com>
2025-05-17 14:25:58 +0900

    informationのアニメーション完了

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,17 @@ jQuery(function ($) {
  ************************/
 
 //Splideの初期化
-const fvSplide = new Splide(".js-fv-splide").mount();
+// const fvSplide = new Splide(".js-fv-splide").mount();
+// Splideの初期化
+document.addEventListener("DOMContentLoaded", function () {
+  const splideElement = document.querySelector(".js-fv-splide");
+  if (splideElement) {
+    console.log("Splide element found:", splideElement);
+    new Splide(splideElement).mount();
+  } else {
+    console.warn("Splide element not found. Skipping initialization.");
+  }
+});
 
 let splideOption = {
   //   loop: true,
@@ -29,7 +39,7 @@ let splideOption = {
 };
 new Splide(".js-fv-splide", splideOption).mount();
 
-const campaignSplide = new Splide(".js-campaign-splide").mount();
+// const campaignSplide = new Splide(".js-campaign-splide").mount();
 
 /************************
  * drawer
@@ -67,4 +77,47 @@ jQuery('a[href^="#"]').on("click", function (e) {
   //   1000,
   //   "swing" // swing or liner
   // );
+});
+
+/************************
+ * 画像の出現アニメーション
+ ************************/
+//要素の取得とスピードの設定
+var box = $(".colorbox"), // .colorboxというクラスを持った要素を全部集めて、「box」として覚えておく
+  speed = 700; // アニメーションにかかる時間（700ミリ秒＝0.7秒）
+
+$(document).ready(function () {
+  var box = $(".colorbox");
+  console.log(box.length); // .colorbox要素の数を確認
+  box.each(function (index) {
+    console.log("No." + index + " の .colorbox:", this); // 各要素をログ出力
+  });
+});
+
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function () {
+  //   console.log("each文の中");
+  //   alert.log("each文の中");
+  // 画像の枠（this）に、色のついた四角（.color）を追加するよ
+  $(this).append('<div class="color"></div>');
+  var color = $(this).find(".color"),
+    image = $(this).find("img");
+  var counter = 0; // 何回動いたか数えるカウンター（0ならまだ動いてない）
+
+  image.css("opacity", "0");
+  color.css("width", "0%");
+  //inviewを使って背景色が画面に現れたら処理をする
+  color.on("inview", function () {
+    if (counter == 0) {
+      $(this)
+        .delay(200) // 少し（0.2秒）待ってから
+        .animate({ width: "100%" }, speed, function () {
+          image.css("opacity", "1");
+          $(this).css({ left: "0", right: "auto" });
+          $(this).animate({ width: "0%" }, speed); // 右側から色が引っ込んでいく
+        });
+      counter = 1;
+      console.log("発火");
+    }
+  });
 });
