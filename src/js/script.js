84f68111c823a65639e25c30ea8696652eb09346
mar <137989121+mar5608@users.noreply.github.com>
2025-05-27 23:38:36 +0900

    top/header/drawer sp/pcピクパ完了

--- src/js/script.js
+++ src/js/script.js
@@ -149,7 +149,7 @@ let splideOptionCampaign = {
   type: "loop", // ループを有効にする
   gap: 40, // スライド間の隙間（ピクセル）
   fixedWidth: 334, // 固定幅を設定（334px）
-  fixedHeight: 445, // 高さ
+  // fixedHeight: 445, // 高さ
   autoplay: false, // 自動再生を有効にする
   interval: 4000, // 自動再生の間隔（ミリ秒）
   speed: 400, // スライド切り替えの速度（ミリ秒）
@@ -163,7 +163,7 @@ let splideOptionCampaign = {
       perPage: 1.22, // 768px未満の表示枚数
       gap: 20, // スライド間の隙間（ピクセル）
       fixedWidth: 280, // 固定幅を設定（280px）
-      fixedHeight: 336, // 高さ
+      // fixedHeight: auto, // 高さ
       arrows: false, // 左右の矢印ボタンも不要であればfalseに
     },
   },
