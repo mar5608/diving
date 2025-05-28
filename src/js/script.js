95b9b79f1fe141dc0b0a9dd96a01f9132e26db65
mar <137989121+mar5608@users.noreply.github.com>
2025-05-28 14:15:32 +0900

    pagetop調整

--- src/js/script.js
+++ src/js/script.js
@@ -75,6 +75,28 @@ $(window).on("load", function () {
   //=====ここまでローディングエリア（loadingエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
 });
 
+/* ****************************************
+topに戻る
+**************************************** */
+jQuery(window).on("scroll", function () {
+  if (100 < jQuery(window).scrollTop()) {
+    jQuery("#js-pagetop").addClass("is-show");
+  } else {
+    jQuery("#js-pagetop").removeClass("is-show");
+  }
+});
+
+// #page-topをクリックした際の設定
+$("#js-pagetop").click(function () {
+  $("body,html").animate(
+    {
+      scrollTop: 0, //ページトップまでスクロール
+    },
+    500
+  ); //ページトップスクロールの速さ。数字が大きいほど遅くなる
+  return false; //リンク自体の無効化
+});
+
 /************************
  * drawer
  ************************/
